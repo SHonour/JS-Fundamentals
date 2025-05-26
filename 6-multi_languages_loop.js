78adc83f756eef60e055dcf9edeab3602a6b6f2a
Honour Shalom <shalomhonour1@gmail.com>
2025-05-26 23:24:45 +0100

    file 07

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,10 @@
-let myArray = ["C is fun", "Python is cool", "JavaScript is amazing"]
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let i = 0;
+let output = "";
 
-for (let i = 0; i < array.length; i++) {
-    console.log(myArray[i]);
-    
+while (i < messages.length) {
+  output += messages[i] + (i < messages.length - 1 ? "\n" : "");
+  i++;
 }
+
+console.log(output);
